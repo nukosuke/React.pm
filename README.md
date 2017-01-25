3db6043d4c133d6476d50f8454d195c62cc9cce4
nukosuke <nukosuke@shibuya.tech>
2017-01-25 17:14:57 +0900

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+React - It's new $module
+
+# SYNOPSIS
+
+    use React;
+
+# DESCRIPTION
+
+React is ...
+
+# LICENSE
+
+Copyright (C) nukosuke.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+nukosuke <nukosuke@shibuya.tech>
---
+++ lib/React.pm
@@ -0,0 +1,39 @@
+package React;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+React - It's new $module
+
+=head1 SYNOPSIS
+
+    use React;
+
+=head1 DESCRIPTION
+
+React is ...
+
+=head1 LICENSE
+
+Copyright (C) nukosuke.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+nukosuke E<lt>nukosuke@shibuya.techE<gt>
+
+=cut
+
